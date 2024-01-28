c5dee6d3cd3b2b281fe723444c55ec6049823ba4
Nikita Melnikov <105976166+NikitaMelnikovq@users.noreply.github.com>
2024-01-28 13:07:01 +0500

    Delete bot_keyboard.py

--- bot_keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-
-kb = [
-    [KeyboardButton(text="/check")],
-    [KeyboardButton(text="/updates_on")],
-    [KeyboardButton(text="/updates_off")],
-    [KeyboardButton(text="/info")],
-]
-keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
